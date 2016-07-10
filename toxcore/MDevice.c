fc81c3bcc6ec30bc996a2ba1ad9dc4f5d5fb727b
endoffile78 <endoffile78@yahoo.com>
2016-07-10 13:59:26 -0500

    change mdev->devices_count to mdev->devices[dev_num].sync_friendlist_size

--- toxcore/MDevice.c
+++ toxcore/MDevice.c
@@ -503,7 +503,7 @@ static int sync_friend_recived(Tox *tox, uint32_t dev_num, uint8_t *real_pk, boo
             if (device) {
                 /* error here, can't add a device_pk when we already know this friend */
                     /* corner case, handling pre-existing friends that need to be grouped together */
-                if (mdev > mdev->devices_count) {
+                if (mdev > mdev->devices[dev_num].sync_friendlist_size) {
                     if (!send_mdev_sync_packet(tox, dev_num, MDEV_SYNC_CONTACT_ERROR)) {
                         decon_sync(mdev, dev_num);
                     }
@@ -530,7 +530,7 @@ static int sync_friend_recived(Tox *tox, uint32_t dev_num, uint8_t *real_pk, boo
             if (!device) {
                 /* A friend already controls this device, but our peer says this is only a friend */
                     /* corner case, handling pre-existing friends that need to be grouped together */
-                if (dev_num <= mdev->devices_count) {
+                if (dev_num <= mdev->devices[dev_num].sync_friendlist_size) {
                     if (!send_mdev_sync_packet(tox, dev_num, MDEV_SYNC_CONTACT_ERROR)) {
                         decon_sync(mdev, dev_num);
                     }
